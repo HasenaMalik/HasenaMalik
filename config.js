9cf37a8d1fa95ca3c96e577c4d2ef4e19747174a
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2024-06-20 21:04:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,82 @@
-
+{
+    "language": "en",
+    "DeveloperMode": false,
+    "autoCreateDB": true,
+    "iconUnsend": {
+        "status": true,
+        "icon": "😠"
+    },
+    "notiGroup": true,
+    "NOTIFICATION": false,
+    "allowInbox": true,
+    "commandDisabled": [],
+    "eventDisabled": [],
+    "BOTNAME": "Zoya Malik",
+    "AMDIN_NAME": "Zoya Malik",
+    "FACEBOOK_ADMIN": "m.me/tntxtrick",
+    "PREFIX": "#",
+    "ADMINBOT": [
+        "61559376917911",
+        "",
+        ""
+    ],
+    "NDH": [
+        ""
+    ],
+    "DATABASE": {
+        "sqlite": {
+            "storage": "antists.sqlite"
+        }
+    },
+    "APPSTATEPATH": "appstate.json",
+    "FCAOption": {
+        "forceLogin": true,
+        "listenEvents": true,
+        "pauseLog": true,
+        "logLevel": "error",
+        "selfListen": false,
+        "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1"
+    },
+    "version": "1.2.14",
+    "menu": {
+        "autoUnsend": {
+            "status": true,
+            "timeOut": 60
+        }
+    },
+    "log": {
+        "enable": true
+    },
+    "daily": {
+        "cooldownTime": 43200000,
+        "rewardCoin": 5000000
+    },
+    "spamcmt": {
+        "spamDelay": 1.5
+    },
+    "test": {
+        "spamDelay": 1.5
+    },
+    "cave": {
+        "cooldownTime": 1200000
+    },
+    "work": {
+        "cooldownTime": 1200000
+    },
+    "help": {
+        "autoUnsend": true,
+        "delayUnsend": 20
+    },
+    "help2": {
+        "autoUnsend": true,
+        "delayUnsend": 500
+    },
+    "slapv2": {
+        "APIKEY": ""
+    },
+    "adminNoti": {
+        "autoUnsend": true,
+        "sendNoti": true,
+        "timeToUnsend": 10
+    }
+      }
